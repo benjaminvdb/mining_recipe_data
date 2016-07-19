f29b02e90ea51722d0f6bfffa30fe9ebc3a0712b
Benjamin van der Burgh <benjamin@wearelandscape.nl>
2016-07-19 15:42:48 +0700

    Added recipe loader

--- recipe.py
+++ recipe.py
@@ -1,4 +1,12 @@
 import numpy as np
 
+from toolbox.path import folder_apply
+
+
 def load_recipe(filename):
     return np.load(filename)['arr_0'][()]
+
+
+def get_recipes(directory, ignore=None):
+    recipes = folder_apply(load_recipe, directory)
+    return filter(lambda recipe: recipe['name'] != u'Johnsonville\xae Three Cheese Italian Style Chicken Sausage Skillet Pizza', recipes)
