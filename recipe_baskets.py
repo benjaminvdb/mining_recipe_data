1af5de5303610d732a1f0b4bd6c1d0fe5c63ac16
Benjamin van der Burgh <benjamin@wearelandscape.nl>
2016-07-19 15:51:58 +0700

    Forgot to add mapping file as argument

--- recipe_baskets.py
+++ recipe_baskets.py
@@ -11,22 +11,24 @@ from recipe import get_recipes
 
 
 if __name__ == '__main__':
-    input_type = PathType(exists=True, type='dir')
-    output_type = argparse.FileType('w')
+    recipe_type = PathType(exists=True, type='dir')
+    mapping_type = argparse.FileType('r')
+    basket_type = argparse.FileType('w')
 
     parser = argparse.ArgumentParser(description='Figure out the normalized ingredients from AllRecipes')
-    parser.add_argument('recipes', type=input_type, help='input directory')
-    parser.add_argument('basket', type=output_type, help='output file')
+    parser.add_argument('recipes', type=recipe_type, help='input directory with recipes')
+    parser.add_argument('mapping', type=mapping_type, help='input file holding the mapping')
+    parser.add_argument('basket', type=basket_type, help='output basket file')
 
     args = parser.parse_args()
 
     recipes = get_recipes(args.recipes)
 
     print("Loading mapper...")
-    mapper = dict(filter(lambda x: x is not None, np.load('ingredient_mappings.npz')['arr_0'][()]))
+    mapper = dict(filter(lambda x: x is not None, np.load(args.mapping)['arr_0'][()]))
     print("Finished loading mapper.")
 
-    with open(args.output, 'w') as fp:
+    with open(args.basket, 'w') as fp:
         for recipe in tqdm(recipes):
             itemset = []
             for ingredient in recipe['ingredients']:
