c5ea06de0008e2baa2b2d0fd987de2c70cb265e7
Benjamin van der Burgh <benjamin@wearelandscape.nl>
2016-07-21 13:28:51 +0700

    Added multiprocessing to review parsing

--- parse_reviews.py
+++ parse_reviews.py
@@ -3,6 +3,7 @@
 import os
 import glob
 import argparse
+import multiprocessing
 
 from tqdm import tqdm
 from bs4 import BeautifulSoup
@@ -12,16 +13,25 @@ from toolbox.argparse.actions import readable_dir, writable_file
 
 reviews = []
 
-def parse_file(filename):
+def parse_file(filename, skip_missing=True):
     reviews = []
     with open(filename) as fp:
         soup = BeautifulSoup(fp, 'html5lib')
 
         for review in soup(attrs={'itemprop': 'review'}):
             id_ = os.path.splitext(os.path.basename(filename))[0]
-            author = review.find(attrs={'itemprop': 'author'}).text.strip()
-            rating = review.find(attrs={'itemprop': 'ratingValue'})['content']
-            date = review.find(attrs={'itemprop': 'dateCreated'})['content']
+
+            author_el = review.find(attrs={'itemprop': 'author'})
+            rating_el = review.find(attrs={'itemprop': 'ratingValue'})
+            date_el = review.find(attrs={'itemprop': 'dateCreated'})
+
+            # Review is complete or we're fine using None as missing value
+            if (author_el and rating_el and date_el) or not skip_missing:
+                author = author_el.text.strip() if author_el else None
+                rating = rating_el['content'] if rating_el else None
+                date = date_el['content'] if date_el else None
+            else:  # Otherwise, we just skip the review
+                continue
 
             reviews.append((id_, author, rating, date))
     return reviews
@@ -38,13 +48,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parse reviews from HTML')
     parser.add_argument('input', action=readable_dir, help='input directory with reviews')
     parser.add_argument('output', action=writable_file, help='output TSV file')
+    parser.add_argument('--skip-missing', action='store_true', help='skip review if there are missing values')
+    parser.add_argument('--pool-size', default=multiprocessing.cpu_count(), help='number of processors to use')
 
     args = parser.parse_args()
 
     filenames = glob.glob(os.path.join(args.input, '*.html'))
 
+    p = multiprocessing.Pool(args.pool_size)
+
     reviews = []
-    for filename in tqdm(filenames):
-        reviews.extend(parse_file(filename))
+    for review in tqdm(p.imap_unordered(parse_file, filenames)):
+        reviews.extend(review)
 
     save_file(args.output)
