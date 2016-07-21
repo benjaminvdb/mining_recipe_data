f60ca56a7ce96e61271a4ba368b5abf5e9c027e8
Benjamin van der Burgh <benjamin@wearelandscape.nl>
2016-07-21 13:39:03 +0700

    Forgot to set default for --skip-missing

--- parse_reviews.py
+++ parse_reviews.py
@@ -45,6 +45,7 @@ if __name__ == '__main__':
     parser.add_argument('output', action=writable_file, help='output TSV file')
     parser.add_argument('--skip-missing', action='store_true', help='skip review if there are missing values')
     parser.add_argument('--pool-size', default=multiprocessing.cpu_count(), help='number of processors to use')
+    parser.set_defaults(skip_missing=False)
 
     args = parser.parse_args()
 
